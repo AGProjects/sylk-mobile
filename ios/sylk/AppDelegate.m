bf87a1d7cdd8204616487c939dc84fe7d362a250
Dan Jenkins <dan@nimblea.pe>
2020-04-02 22:26:11 +0100

    make ios load properly, will test thoroughly now

--- ios/sylk/AppDelegate.m
+++ ios/sylk/AppDelegate.m
@@ -12,6 +12,12 @@
 #import <React/RCTRootView.h>
 #import <WebRTC/RTCLogging.h>
 #import <React/RCTLog.h>
+#import <React/RCTLinkingManager.h>
+#import <PushKit/PushKit.h>
+@import Firebase;
+#import "RNCallKeep.h"
+// #import "RNNotifications.h"
+#import "RNVoipPushNotificationManager.h"
 
 @implementation AppDelegate
 
@@ -20,7 +26,9 @@
 
   // RCTSetLogThreshold(RCTLogLevelInfo - 1);
   // RTCSetMinDebugLogLevel(RTCLoggingSeverityInfo);
-
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
@@ -37,6 +45,23 @@
   return YES;
 }
 
+- (BOOL)application:(UIApplication *)application
+continueUserActivity:(NSUserActivity *)userActivity
+  restorationHandler:(void(^)(NSArray * __nullable restorableObjects))restorationHandler
+{
+  BOOL handled = [RNCallKeep application:application
+           continueUserActivity:userActivity
+             restorationHandler:restorationHandler];
+
+  if (!handled) {
+    handled = [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+  }
+
+  return handled;
+}
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
@@ -44,6 +69,26 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+// Handle updated push credentials
+- (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)credentials forType:(NSString *)type {
+  // Register VoIP push token (a property of PKPushCredentials) with server
+  [RNVoipPushNotificationManager didUpdatePushCredentials:credentials forType:(NSString *)type];
+}
+
+// Handle incoming pushes
+- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(NSString *)type withCompletionHandler:(void (^)(void))completion{
+  // Process the received push
+  [RNVoipPushNotificationManager didReceiveIncomingPushWithPayload:payload forType:(NSString *)type];
+  NSLog(@"Got a PUSHKIT NOTIFICATION");
+  // Retrieve information like handle and callerName here
+  NSString *uuid = [payload.dictionaryPayload valueForKey:@"callUUID"];
+  NSString *callerName = [payload.dictionaryPayload valueForKey:@"fromName"];
+  NSString *handle = [payload.dictionaryPayload valueForKey:@"fromNumber"];
+
+  [RNCallKeep reportNewIncomingCall:uuid handle:handle handleType:@"number" hasVideo:false localizedCallerName:callerName fromPushKit: YES];
+  completion();
 }
 
 @end
