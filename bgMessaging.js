d22fd79701d1bea087c1f7e3e08f27d8de42cf2f
Dan Jenkins <dan@nimblea.pe>
2020-07-06 21:24:25 +0100

    fix missed merge conflict

--- bgMessaging.js
+++ bgMessaging.js
@@ -6,17 +6,12 @@ const { SylkNative } = NativeModules;
 const logger = new Logger('bgMessaging');
 
 export default async (remoteMessage) => {
-<<<<<<< Updated upstream
-    console.log("Data notification received in state ", AppState.currentState, remoteMessage, SylkNative);
-    if(AppState.currentState == "background" && (remoteMessage.data.event === 'incoming_session' || remoteMessage.data.event === 'incoming_conference_request')) {
-=======
     logger.debug("Data notification received in state " + AppState.currentState, remoteMessage, SylkNative);
 
     if(
         AppState.currentState == "background" &&
         (remoteMessage.data.event === 'incoming_session' || remoteMessage.data.event === 'incoming_conference_request')
     ) {
->>>>>>> Stashed changes
         //rnn_hasRNHost indicates if the app has a react native host when the notification was received. This is used to determine if the app was in the background or dead. If it was dead then launch the main activity to wake enough of the phone to be useful.
         console.log('Wake up from push');
         SylkNative.launchMainActivity();
