05331bd1e5923e4f0af335a78cd2255789aaeea0
Adrian Georgescu <ag@ag-projects.com>
2020-07-26 12:09:44 -0300

    Update Android background functions

--- bgCalling.js
+++ bgCalling.js
@@ -2,6 +2,6 @@
 import { Linking } from 'react-native';
 
 export default async ({ name, callUUID, handle }) => {
-    Linking.openURL(`sylk://dialer/call/${callUUID}/${handle}/${name}`)
+    Linking.openURL(`sylk://outgoing/call/${callUUID}/${handle}/${name}`)
     return Promise.resolve();
 }
--- bgMessaging.js
+++ bgMessaging.js
@@ -3,7 +3,7 @@ import { NativeModules, AppState } from 'react-native';
 const { SylkNative } = NativeModules;
 
 export default async (remoteMessage) => {
-    console.log("Data notification received in state " + AppState.currentState, remoteMessage, SylkNative);
+    console.log('Push notification received', remoteMessage.data.event);
 
     if (AppState.currentState == "background" &&
         (remoteMessage.data.event === 'incoming_session' || remoteMessage.data.event === 'incoming_conference_request')
@@ -11,19 +11,20 @@ export default async (remoteMessage) => {
 
         let event = remoteMessage.data.event;
         let callUUID = remoteMessage.data['session-id'];
-        let room = remoteMessage.data['to_uri']
+        let to = remoteMessage.data['to_uri']
         let from = remoteMessage.data['from_uri']
+        let url;
 
         if (event === 'incoming_conference_request') {
-            let url = 'sylk://dialer/conference/' + callUUID + '/' + room + '/' + from;
-        } else if (event === 'incoming_conference_request') {
-            let url = 'sylk://dialer/call/' + callUUID + '/' + from + '/' + from;
+            url = 'sylk://outgoing/conference/' + callUUID + '/' + from + '/' + to;
+        } else if (event === 'incoming_session') {
+            url = 'sylk://incoming/call/' + callUUID + '/' + from + '/' + to;
         }
 
-        console.log('Wake up from push', url);
-
-        SylkNative.launchMainActivity(url);
-
+        if (url) {
+            console.log('Wake up from push with URL', url);
+            SylkNative.launchMainActivity(url);
+        }
     }
 
     return Promise.resolve();
