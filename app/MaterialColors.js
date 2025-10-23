27c7b33014598d09092d1234aa07bc1b1e955dc7
Adrian Georgescu <ag@ag-projects.com>
2025-10-23 09:33:48 -0300

    Update colors

--- app/MaterialColors.js
+++ app/MaterialColors.js
@@ -1,19 +1,16 @@
-'use strict';
-
-const colors = require('material-ui/colors');
-const murmur = require('murmurhash-js');
+// generateColor.js
+import * as colors from './colors';
+import murmur from 'murmurhash-js';
 
 // Available material design colors
 const availableColors = [
-    colors.red,         colors.pink,    colors.purple,      colors.deepPurple,
-    colors.indigo,      colors.blue,    colors.lightBlue,   colors.cyan,
-    colors.teal,        colors.green,   colors.lightGreen,  colors.lime,
-    colors.yellow,      colors.amber,   colors.orange,      colors.deepOrange,
-    colors.brown,       colors.grey,    colors.blueGrey
+  colors.red,         colors.pink,    colors.purple,      colors.deepPurple,
+  colors.indigo,      colors.blue,    colors.lightBlue,   colors.cyan,
+  colors.teal,        colors.green,   colors.lightGreen,  colors.lime,
+  colors.yellow,      colors.amber,   colors.orange,      colors.deepOrange,
+  colors.brown,       colors.grey,    colors.blueGrey
 ];
 
-function generateColor(text) {
-    return availableColors[(murmur.murmur3(text) % availableColors.length)];
+export function generateColor(text) {
+  return availableColors[murmur.murmur3(text) % availableColors.length];
 }
-
-exports.generateColor = generateColor;
