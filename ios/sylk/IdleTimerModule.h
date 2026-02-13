a1d5addba2126fc55441b5b48a55ab93cc416988
Adrian Georgescu <ag@ag-projects.com>
2026-02-13 09:26:06 -0300

    Added missing iOS files

---
+++ ios/sylk/IdleTimerModule.h
@@ -0,0 +1,4 @@
+#import <React/RCTBridgeModule.h>
+
+@interface IdleTimerModule : NSObject <RCTBridgeModule>
+@end
---
+++ ios/sylk/IdleTimerModule.m
@@ -0,0 +1,16 @@
+#import "IdleTimerModule.h"
+#import <UIKit/UIKit.h>
+
+@implementation IdleTimerModule
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(setIdleTimerDisabled:(BOOL)disabled)
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [UIApplication sharedApplication].idleTimerDisabled = disabled;
+    NSLog(@"[sylk_app] idleTimerDisabled = %@", disabled ? @"YES" : @"NO");
+  });
+}
+
+@end
